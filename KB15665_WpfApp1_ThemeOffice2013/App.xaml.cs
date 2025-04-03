5bad66d367e8e6ce58335c16d571f64c68641a79
norikois <33947244+norikois@users.noreply.github.com>
2025-04-03 15:12:53 +0900

    Add files via upload

---
+++ KB15665_WpfApp1_ThemeOffice2013/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KB15665_WpfApp1_ThemeOffice2013
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
